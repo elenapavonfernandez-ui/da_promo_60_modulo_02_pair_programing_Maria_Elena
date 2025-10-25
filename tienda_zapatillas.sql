6a3dd61d3a419bd49cb1d4d737dbdc66f49b8bd3
ElenaP <elenapavonfernandez@gmail.com>
2025-10-25 19:29:36 +0200

    schema zapatillas actualizado

--- tienda_zapatillas.sql
+++ tienda_zapatillas.sql
@@ -50,3 +50,41 @@ CONSTRAINT fk_cliente
    REFERENCES clientes(id_cliente),
 PRIMARY KEY (id_factura)
 );
+
+USE tienda_zapatillas;
+ALTER TABLE zapatillas
+ADD COLUMN marca VARCHAR(45) NOT NULL,
+ADD COLUMN talla INT NOT NULL;
+
+ALTER TABLE empleados
+MODIFY COLUMN salario FLOAT;
+
+ALTER TABLE clientes 
+DROP COLUMN pais;
+ 
+ALTER TABLE facturas 
+ADD COLUMN total FLOAT;
+
+INSERT INTO zapatillas (id_zapatillas, modelo, color, marca, talla)
+VALUES (1, "xqyun", "negro", "nike", 42), (2, "uopmn", "rosa", "nike", 39), (3, "opnyt", "verde", "adidas", 35);
+
+SELECT * 
+  FROM zapatillas;
+ 
+ INSERT INTO empleados (id_empleado, nombre, tienda, salario, fecha_incorporacion)
+ VALUES (2, "Maria", "Sevilla", null, "2001-04-11"), (3, "Esther", "Oviedo", 30165.98, "2000-11-29");
+ 
+ SELECT * 
+  FROM empleados;
+  
+INSERT INTO clientes (id_cliente, nombre, numero_telefono, email, direccion, ciudad, provincia, codigo_postal)
+VALUES (1, "Monica", 473953, "monica@gmail.com", "calle felicidad", "Mostoles", "Madrid", 28176), (2, "Lorena", 275293, "lorena@email.com", "calle alegria", "Barcelona", "Barcelona", 12345), (3, "Carmen", 274502, "carmen@email.com", "Calle del color", "Vigo", "Pontevedra", 23456);
+
+SELECT * 
+  FROM clientes;
+  
+INSERT INTO facturas (id_factura, numero_factura, fecha, zapatilla, empleado, cliente, total)
+VALUES (1, 123, "2001-12-11", 1, 2, 1, 54.98), (2, 1234, "2005-05-23", 1, 1, 3, 89.91), (3, 12345, "2015-09-18", 2, 3, 3, 76.23);  
+
+SELECT * 
+  FROM facturas; 
